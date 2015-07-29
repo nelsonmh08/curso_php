499c5043dca927b0c808444e08eeebf5733a7d97
Nelson <nelsonmh08@gmail.com>
2015-07-28 19:40:24 -0500

    Agregue index.php
    Conexion a BD

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-	echo"Hola Muncdo";
-	echo"Curso de PHP Avanzado";
-	echo"2015";
-	echo"FIEC";
+	echo"CONEXION A LA BASE DE DATOS DEMO";
+	$URL="sql5.freemysqlhosting.net";
+	$database="sql585296";
+	$usuario="sql585296";
+	$password="yX9!zJ5%";
+	$port="3306";
+	// CREAR CONNECCION 
+	$conexio=mysql_connect($URL,$usuario,$password,$database,$port);
+	if(!$conexion) { die ("FINALIZO LA CONEXION");
+}
+echo "CONEXION EXITOSA";
 ?>
